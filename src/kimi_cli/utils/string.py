659d49438167c5cf7ba7cacd4f51dc9f152dc27d
Richard Chien <stdrc@outlook.com>
2025-09-16 10:56:02 +0800

    fix: remove newline in tool call status
    Signed-off-by: Richard Chien <stdrc@outlook.com>

--- src/kimi_cli/utils/string.py
+++ src/kimi_cli/utils/string.py
@@ -1,5 +1,12 @@
-def shorten_middle(text: str, width: int) -> str:
+import re
+
+_NEWLINE_RE = re.compile(r"[\r\n]+")
+
+
+def shorten_middle(text: str, width: int, remove_newline: bool = True) -> str:
     """Shorten the text by inserting ellipsis in the middle."""
     if len(text) <= width:
         return text
+    if remove_newline:
+        text = _NEWLINE_RE.sub(" ", text)
     return text[: width // 2] + "..." + text[-width // 2 :]
